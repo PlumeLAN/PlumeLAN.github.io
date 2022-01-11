27434f6509f6208cb424d5d5c3aa40345137b0af
LitterHUI <61373470+PlumeLAN@users.noreply.github.com>
2022-01-11 15:34:10 +0800

    Add files via upload

--- ceshi.js
+++ ceshi.js
@@ -1,7 +1,8 @@
+localStorage.pass1 = "vEyQNRNFjshsfUQCrghhuDfBfa8dx35B";
 function jisuan() {
 	// body...
 	sernum = parseInt(document.getElementById("shebeihao").value);
-	serid = parseInt(document.getElementById("shebeiid").value);
+	serid = parseInt(31);
 	serdata = (document.getElementById("qixian").value);
 	
 	ss = serdata.toString().split("-");
@@ -19,8 +20,25 @@ function jisuan() {
 	var temp3 = document.getElementById("jieguo");
 	temp3.innerHTML = a5 + "-" + a4 + "-" + a3 + "-" + a2 + "-" + a1;
 }
-if(window.localStorage){
- alert('This browser supports localStorage');
-}else{
- alert('This browser does NOT support localStorage');
+function baocun() {
+	// body...
+	usermima = document.getElementById("mima").value; 
+	if (localStorage.pass1 == usermima) {
+		window.location.href="ceshi.html";
+	}
+	else{
+		alert("密码错误");
+	}
 }
+function SaveTxt() {
+        var FileName = '1.txt'; // 文件名称 - 文件名称后添加“.txt”或“.doc”可以保存为对应格式
+        var Content = "gdadahdahsd"; // 文本内容
+
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(Content));
+        element.setAttribute('download', FileName);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }

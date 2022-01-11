20c7cd4e3b2e5f44cefc8b038909451639109c11
LitterHUI <61373470+PlumeLAN@users.noreply.github.com>
2022-01-11 09:38:42 +0800

    Add files via upload

--- ceshi.js
+++ ceshi.js
@@ -1,10 +1,21 @@
 function jisuan() {
 	// body...
-	var temp1 = document.getElementById("shebeihao");
-	var sernum = temp1.innerHTML;
-	var temp2 = document.getElementById("shebeiid");
-	var serid = temp2.innerHTML;
+	sernum = parseInt(document.getElementById("shebeihao").value);
+	serid = parseInt(document.getElementById("shebeiid").value);
+	serdata = (document.getElementById("qixian").value);
+	
+	ss = serdata.toString().split("-");
+	sss = ss[0] + ss[1] + ss[2];
+	key = (sernum * serid - 1) * 470;
+	key2 = sernum % 100;
+	data = parseInt(sss + key2);
+	a1 = ((key ^ data) % 100).toString();
+	a2 = Math.trunc((key ^ data) / 100 % 100).toString();
+	a3 = Math.trunc((key ^ data) / 10000 % 100).toString();
+	a4 = Math.trunc((key ^ data) / 1000000 % 100).toString();
+	a5 = Math.trunc((key ^ data) / 100000000).toString();
+	//alert(a5 + "-" + a4 + "-" + a3 + "-" + a2 + "-" + a1);
+	//alert(a2);
 	var temp3 = document.getElementById("jieguo");
-	temp3.innerHTML = "sernum+serid";
-	document.write(sernum);
+	temp3.innerHTML = a5 + "-" + a4 + "-" + a3 + "-" + a2 + "-" + a1;
 }
